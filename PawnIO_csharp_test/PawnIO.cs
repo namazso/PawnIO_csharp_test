227fc57bbe3acfdf4c5301ff20c249b24c760723
namazso <admin@namazso.eu>
2023-06-17 12:43:55 +0200

    Initial commit

---
+++ PawnIO_csharp_test/PawnIO.cs
@@ -0,0 +1,84 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PawnIO_csharp_test
+{
+    internal class PawnIO
+    {
+        [DllImport("kernel32", SetLastError = true)]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("PawnIOLib.dll", PreserveSig = false)]
+        private static extern void pawnio_version(out uint version);
+
+        [DllImport("PawnIOLib.dll", PreserveSig = false)]
+        private static extern void pawnio_open(out IntPtr handle);
+
+        [DllImport("PawnIOLib.dll", PreserveSig = false)]
+        private static extern void pawnio_load(IntPtr handle, byte[] blob, IntPtr size);
+
+        [DllImport("PawnIOLib.dll", CharSet = CharSet.Ansi, PreserveSig = false)]
+        private static extern void pawnio_execute(IntPtr handle, string name, long[] in_array, IntPtr in_size, long[] out_array, IntPtr out_size, out IntPtr return_size);
+
+        [DllImport("PawnIOLib.dll", PreserveSig = false)]
+        private static extern void pawnio_close(IntPtr handle);
+
+        private readonly IntPtr _handle;
+
+        private static bool TryLoadDll()
+        {
+            var pawnioPath = Environment.GetEnvironmentVariable("PAWNIO_ROOT");
+            if (!string.IsNullOrEmpty(pawnioPath))
+            {
+                try
+                {
+                    LoadLibrary(pawnioPath + Path.DirectorySeparatorChar + "PawnIOLib.dll");
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public static uint Version()
+        {
+            TryLoadDll();
+            pawnio_version(out var version);
+            return version;
+        }
+
+        public PawnIO()
+        {
+            TryLoadDll();
+            pawnio_open(out _handle);
+        }
+
+        public PawnIO(byte[] bytes)
+        {
+            TryLoadDll();
+            pawnio_open(out _handle);
+            Load(bytes);
+        }
+
+        ~PawnIO()
+        {
+            if (_handle != IntPtr.Zero)
+                pawnio_close(_handle);
+        }
+
+        public void Load(byte[] bytes)
+        {
+            pawnio_load(_handle, bytes, (IntPtr)bytes.Length);
+        }
+
+        public long[] Execute(string name, long[] input, int outLength)
+        {
+            var outArray = new long[outLength];
+            pawnio_execute(_handle, name, input, (IntPtr)input.Length, outArray, (IntPtr)outArray.Length, out var returnLength);
+            Array.Resize(ref outArray, (int)returnLength);
+            return outArray;
+        }
+    }
+}
---
+++ PawnIO_csharp_test/Program.cs
@@ -0,0 +1,8 @@
+﻿using PawnIO_csharp_test;
+
+Console.WriteLine($"PawnIO version: {PawnIO.Version():x}");
+var pawnIO = new PawnIO();
+var bytes = File.ReadAllBytes("LpcIO.bin");
+pawnIO.Load(bytes);
+var outVals = pawnIO.Execute("ioctl_detect", new long[] { 1, 1 }, 2);
+Console.WriteLine($"Detected chips: 2E {outVals[0]:x} 4E {outVals[1]:x}");
